39423d1350043226063e7dd83bdbf4ae6c1da220
Wasifur Rahman Efaz <rahmanefazwasifur@gmail.com>
2026-09-23 23:19:04 +0600

    app folder structure updated

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,18 @@
+// app/(public)/layout.tsx
+
+import Navbar from "@/components/landing/Navbar";
+import Footer from "@/components/layout/Footer";
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, DM_Serif_Display, Manrope } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/landing/Navbar";
-import Footer from "@/components/layout/Footer";
 import Providers from "./providers";
 
 const geistSans = Geist({
@@ -32,9 +30,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${dmSerif.variable} ${manrope.variable}  dark h-full antialiased relative`}
     >
       <body className="min-h-full flex flex-col">
-        <Navbar />
         <Providers>{children}</Providers>
-        <Footer />
       </body>
     </html>
   );
